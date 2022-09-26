0190f52aad805a676ed722d1ecaf9750260deffc
yusharnadi <yust.44@gmail.com>
2022-09-27 00:07:02 +0700

    add indicator migration

---
+++ database/migrations/2022_09_26_154526_create_indicators_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('indicators', function (Blueprint $table) {
+            $table->id('indicator_id');
+            $table->string('indicator_code');
+            $table->string('indicator_name');
+            $table->text('indicator_description');
+            $table->tinyInteger('indicator_visibility');
+            $table->tinyInteger('aspect_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('indicators');
+    }
+};
