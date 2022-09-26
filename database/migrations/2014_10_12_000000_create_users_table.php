2eb172db1fde77e4fdf23ae45aff59c1c30ae121
yusharnadi <yust.44@gmail.com>
2022-09-27 00:07:09 +0700

    fix user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('nip')->unique();
+            $table->integer('department_id')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
