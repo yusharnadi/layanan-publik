259f705cd0863aa7d3d2667ecab815d10c09cedf
yusharnadi <yust.44@gmail.com>
2022-09-27 00:04:42 +0700

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-      Copyright &copy; 2022 <div class="bullet"></div> {{config('app.name')}} <a href="{{route('dashboard')}}" target="_blank"> | BPKSDM SINGKAWANG</a>
+      Copyright &copy; 2022 <div class="bullet"></div> {{config('app.name')}} <a href="{{route('dashboard')}}" target="_blank"> | BAGIAN ORGANISASI SETDA KOTA SINGKAWANG</a>
     </div>
     <div class="footer-right">
       V 1.0
