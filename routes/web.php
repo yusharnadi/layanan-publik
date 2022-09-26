b8fb0830e6619a10fbc52177cbcdbddfe736842b
yusharnadi <yust.44@gmail.com>
2022-09-27 00:06:21 +0700

    sync web route

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,14 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DeviceController;
-use App\Http\Controllers\PassengerController;
-use App\Http\Controllers\PersonController;
-use App\Http\Controllers\SliderController;
+use App\Http\Controllers\IndicatorController;
 use App\Http\Controllers\SmsController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\UserMobileController;
-use App\Http\Controllers\UserRoleController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::resource('/sms', SmsController::class);
+Route::resource('/indicator', IndicatorController::class);
 Route::resource('/device', DeviceController::class);
 
 Route::get('/login', [AuthController::class, 'login'])->middleware('guest')->name('login');
@@ -29,10 +25,6 @@ Route::middleware('auth')->group(function () {
     // DELETE ROUTE 
 
     Route::get('/user/{id}/delete', [UserController::class, 'delete'])->name('users.delete');
-    Route::get('/passenger/{id}/delete', [PassengerController::class, 'delete'])->name('passenger.delete');
-    Route::get('/person/{id}/delete', [PersonController::class, 'delete'])->name('person.delete');
-    Route::get('/userMobile/{id}/delete', [UserMobileController::class, 'delete'])->name('userMobile.delete');
-    Route::get('/slider/{id}/delete', [SliderController::class, 'delete'])->name('slider.delete');
 });
 Route::get('/sms/{id}/delete', [SmsController::class, 'delete'])->name('sms.delete');
-Route::get('/device/{id}/delete', [DeviceController::class, 'delete'])->name('device.delete');
+Route::get('/indicator/{id}/delete', [IndicatorController::class, 'delete'])->name('indicator.delete');
