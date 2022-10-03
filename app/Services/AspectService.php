dd8908b3cf86d16c2533467dd7bd33f3cdce1147
yusharnadi <yust.44@gmail.com>
2022-10-04 00:18:20 +0700

    add other func

--- app/Services/AspectService.php
+++ app/Services/AspectService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Aspect;
+use App\Models\Indicator;
 
 class AspectService
 {
@@ -14,5 +15,21 @@ class AspectService
     public function insert(array $data)
     {
         return Aspect::create($data);
+    }
+
+    public function findAllWithIndicator()
+    {
+        $aspects = Aspect::all();
+        $result = [];
+        foreach ($aspects as $aspect) {
+            $indicator_temp = Indicator::where('aspect_id', $aspect->aspect_id)->get();
+            $aspect_temp = [
+                'aspect_id' => $aspect->aspect_id,
+                'aspect_name' => $aspect->aspect_name,
+                'indicator' => $indicator_temp
+            ];
+            array_push($result, $aspect_temp);
+        }
+        return $result;
     }
 }
