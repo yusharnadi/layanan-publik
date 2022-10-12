9aab93d1317a4417938c4ee2726da8e76e91fd36
yusharnadi <yust.44@gmail.com>
2022-10-12 13:42:10 +0700

    done department seeder

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -134,5 +134,250 @@ class DepartmentSeeder extends Seeder
             "department_name" => "BPBD",
             "department_fullname" => "Badan Penanggulangan Bencana Daerah"
         ]);
+
+        Departments::create([
+            "department_name" => "SINGTENG",
+            "department_fullname" => "Kecamatan Singkawang Tengah"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGTARA",
+            "department_fullname" => "Kecamatan Singkawang Tengah"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGTIM",
+            "department_fullname" => "Kecamatan Singkawang Timur"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGBAR",
+            "department_fullname" => "Kecamatan Singkawang Barat"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGSEL",
+            "department_fullname" => "Kecamatan Singkawang Selatan"
+        ]);
+
+        Departments::create([
+            "department_name" => "PAJINTAN",
+            "department_fullname" => "Kelurahan Pajintan"
+        ]);
+
+        Departments::create([
+            "department_name" => "PANGMILANG",
+            "department_fullname" => "Kelurahan Pangmilang"
+        ]);
+
+        Departments::create([
+            "department_name" => "NARAM",
+            "department_fullname" => "Kelurahan Naram"
+        ]);
+
+        Departments::create([
+            "department_name" => "SETAPUK KECIL",
+            "department_fullname" => "Kelurahan Setapuk Kecil"
+        ]);
+
+        Departments::create([
+            "department_name" => "SIJANGKUNG",
+            "department_fullname" => "Kelurahan Sijangkung"
+        ]);
+
+        Departments::create([
+            "department_name" => "MELAYU",
+            "department_fullname" => "Kelurahan Melayu"
+        ]);
+
+        Departments::create([
+            "department_name" => "BUKIT BATU",
+            "department_fullname" => "Kelurahan Bukit Batu"
+        ]);
+
+        Departments::create([
+            "department_name" => "ROBAN",
+            "department_fullname" => "Kelurahan Roban"
+        ]);
+
+        Departments::create([
+            "department_name" => "MAYA SOPA",
+            "department_fullname" => "Kelurahan Maya Sopa"
+        ]);
+
+        Departments::create([
+            "department_name" => "SAGATANI",
+            "department_fullname" => "Kelurahan Sagatani"
+        ]);
+
+        Departments::create([
+            "department_name" => "S.RASAU",
+            "department_fullname" => "Kelurahan Sungai Rasau"
+        ]);
+
+        Departments::create([
+            "department_name" => "S.BULAN",
+            "department_fullname" => "Kelurahan Sungai Bulan"
+        ]);
+
+        Departments::create([
+            "department_name" => "SEDAU",
+            "department_fullname" => "Kelurahan Sedau"
+        ]);
+
+        Departments::create([
+            "department_name" => "KUALA",
+            "department_fullname" => "Kelurahan Kuala"
+        ]);
+
+        Departments::create([
+            "department_name" => "TENGAH",
+            "department_fullname" => "Kelurahan Tengah"
+        ]);
+
+        Departments::create([
+            "department_name" => "CONDONG",
+            "department_fullname" => "Kelurahan Condong"
+        ]);
+
+        Departments::create([
+            "department_name" => "PASIRAN",
+            "department_fullname" => "Kelurahan Pasiran"
+        ]);
+
+        Departments::create([
+            "department_name" => "BAGAK SAHWA",
+            "department_fullname" => "Kelurahan Bagak Sahwa"
+        ]);
+
+        Departments::create([
+            "department_name" => "S.WIE",
+            "department_fullname" => "Kelurahan Sungai Wie"
+        ]);
+
+        Departments::create([
+            "department_name" => "SEKIP LAMA",
+            "department_fullname" => "Kelurahan Sekip Lama"
+        ]);
+
+        Departments::create([
+            "department_name" => "JAWA",
+            "department_fullname" => "Kelurahan Jawa"
+        ]);
+
+        Departments::create([
+            "department_name" => "S.GARAM HILIR",
+            "department_fullname" => "Kelurahan Sungai Garam Hilir"
+        ]);
+
+        Departments::create([
+            "department_name" => "SETAPUK BESAR",
+            "department_fullname" => "Kelurahan Setapuk Besar"
+        ]);
+
+        Departments::create([
+            "department_name" => "SEMELAGI KECIL",
+            "department_fullname" => "Kelurahan Semelagi Kecil"
+        ]);
+
+        Departments::create([
+            "department_name" => "NYARUMKOP",
+            "department_fullname" => "Kelurahan Nyarumkop"
+        ]);
+
+        Departments::create([
+            "department_name" => "SANGGAU KULOR",
+            "department_fullname" => "Kelurahan Sanggau Kulor"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGTARA I",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Utara I"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGTARA II",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Utara II"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGTIM I",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Timur I"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGTIM II",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Timur II"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGSEL I",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Selatan I"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGSEL II",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Selatan II"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGBAR I",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Barat I"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGBAR II",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Barat II"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGTENG I",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Tengah I"
+        ]);
+
+        Departments::create([
+            "department_name" => "SINGTENG II",
+            "department_fullname" => "UPT PUSKESMAS Singkawang Tengah II"
+        ]);
+
+        Departments::create([
+            "department_name" => "METROLOGI LEGAL",
+            "department_fullname" => "UPT Metrologi Legal"
+        ]);
+
+        Departments::create([
+            "department_name" => "RUMAH POTONG HEWAN",
+            "department_fullname" => "UPT Rumah Potong Hewan"
+        ]);
+
+        Departments::create([
+            "department_name" => "PENGUJIAN KENDARAAN BERMOTOR",
+            "department_fullname" => "UPT Pengujian Kendaraan Bermotor"
+        ]);
+
+        Departments::create([
+            "department_name" => "PENGELOLAAN SAMPAH",
+            "department_fullname" => "UPT Pengelolaan Sampah"
+        ]);
+
+        Departments::create([
+            "department_name" => "PUSAT KESEHATAN HEWAN TERPADU",
+            "department_fullname" => "UPT Pusat Kesehatan Hewan Terpadu"
+        ]);
+
+        Departments::create([
+            "department_name" => "LAB LINGKUNGAN",
+            "department_fullname" => "UPT Laboratorium Lingkungan"
+        ]);
+
+        Departments::create([
+            "department_name" => "LLK UKM",
+            "department_fullname" => "UPT Loka Latihan Kerja Usaha Kecil dan Menengah"
+        ]);
+
+        Departments::create([
+            "department_name" => "PEMELIHARAAN JALAN JEMBATAN ALAT BERAT",
+            "department_fullname" => "UPT Pemeliharaan Jalan, Jembatan dan Alat Berat"
+        ]);
     }
 }
